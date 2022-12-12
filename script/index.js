6727e4a4b87e4fd4d4e0486a80f0a23825df7f94
Andrej Kudinov <kudinov.andrej@list.ru>
2022-12-12 12:02:44 +0300

    версия до правок

---
+++ script/index.js
@@ -0,0 +1,37 @@
+const aboutButton = document.querySelector('.profile__edit-button');
+const aboutPopap = document.querySelector('.popap');
+const aboutButtonClose = document.querySelector('.popap__button-close');
+const aboutformName = document.querySelector('.popap__form-name');
+const aboutformProfession = document.querySelector('.popap__form-profession');
+const aboutSave = document.querySelector('.popap__button');
+const aboutName = document.querySelector('.profile__name');
+const aboutProfession = document.querySelector('.profile__profession');
+const formElement = document.querySelector('.popap__form');
+
+
+aboutButton.addEventListener('click', () => {
+    aboutPopap.classList.add('popap__popap_on');
+});
+
+aboutButtonClose.addEventListener('click', () => {
+    aboutPopap.classList.remove('popap__popap_on');
+});
+
+
+
+function handleFormSubmit (evt) {
+    evt.preventDefault(); 
+
+    aboutName.textContent = aboutformName.value;
+    aboutProfession.textContent = aboutformProfession.value;
+
+};
+
+aboutSave.addEventListener('click', () => {
+    aboutPopap.classList.remove('popap__popap_on');
+});
+
+
+formElement.addEventListener('submit', handleFormSubmit); 
+
+
