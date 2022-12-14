e4d58b8615653c666c715dbb656776e05a813e61
Andrej Kudinov <kudinov.andrej@list.ru>
2022-12-14 06:05:44 +0300

    исправления 2-я итерация

--- script/index.js
+++ script/index.js
@@ -1,37 +1,35 @@
 const aboutButton = document.querySelector('.profile__edit-button');
 const aboutPopap = document.querySelector('.popap');
 const aboutButtonClose = document.querySelector('.popap__button-close');
-const aboutformName = document.querySelector('.popap__form-name');
-const aboutformProfession = document.querySelector('.popap__form-profession');
-const aboutSave = document.querySelector('.popap__button');
+const aboutformName = document.querySelector('.popap__form-info_name');
+const aboutformProfession = document.querySelector('.popap__form-info_profession');
 const aboutName = document.querySelector('.profile__name');
 const aboutProfession = document.querySelector('.profile__profession');
 const formElement = document.querySelector('.popap__form');
 
 
-aboutButton.addEventListener('click', () => {
-    aboutPopap.classList.add('popap_opened');
-});
+function popapOpen() {
+  aboutPopap.classList.add('popap_opened');
+  aboutformName.value = aboutName.textContent;
+  aboutformProfession.value = aboutProfession.textContent;
+}
 
-aboutButtonClose.addEventListener('click', () => {
+function popapClose() {
     aboutPopap.classList.remove('popap_opened');
-});
-
-
+  
+}
 
 function handleFormSubmit (evt) {
     evt.preventDefault(); 
-
     aboutName.textContent = aboutformName.value;
     aboutProfession.textContent = aboutformProfession.value;
+    popapClose();
 
 };
 
-aboutSave.addEventListener('click', () => {
-    aboutPopap.classList.remove('popap_opened');
-});
-
-
+aboutButtonClose.addEventListener('click', popapClose);
+aboutButton.addEventListener('click', popapOpen);
 formElement.addEventListener('submit', handleFormSubmit); 
+
 
 
