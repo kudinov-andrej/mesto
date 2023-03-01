a55cf3728607aad7729685136f8a0a8d14189c74
Andrej Kudinov <kudinov.andrej@list.ru>
2023-03-02 02:06:07 +0300

    8 спринт 1 проверка

--- src/script/index.js
+++ src/script/index.js
@@ -1,5 +1,5 @@
+import '../pages/index.css';
 import Section from "../components/Section.js";
-
 import Card from "../components/Card.js";
 import UserInfo from "../components/UserInfo.js";
 import FormValidator from "../components/FormValidator.js";
