1142a6dfab162ee253778d060a2adee3e89137d6
Andrej Kudinov <kudinov.andrej@list.ru>
2023-03-19 01:48:29 +0300

    8 спринт 2 проверка

--- src/components/PopupWithFormDeleteCard.js
+++ src/components/PopupWithFormDeleteCard.js
@@ -8,7 +8,7 @@ export default class PopupWithFormDeleteCard extends Popup {
   }
 
   setFormSubmitHandler(token) {
-      this.setFormSubmitHandler = token;
+      this.formSubmitHandler = token;
       
   }
 
@@ -16,7 +16,7 @@ export default class PopupWithFormDeleteCard extends Popup {
       super.setEventListeners();
       this._form.addEventListener('submit', (event) => {
           event.preventDefault();
-          this.setFormSubmitHandler();
+          this.formSubmitHandler();
       });
   }
 }
